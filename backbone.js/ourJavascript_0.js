fbc15044ce1fe0f8639952e369d2c31c851902a1
Kim MinSeop <kmsp91@gmail.com>
2017-11-16 09:01:15 +0900

    Update ourJavascript_0.js

--- backbone.js/ourJavascript_0.js
+++ backbone.js/ourJavascript_0.js
@@ -1,9 +1,12 @@
-WebsiteModel = Backbone.Model.extend({
+WebsiteModel = Backbone.Model.extend({ //  모델생성, 생성자함수
   initialize: function() {
       alert("Hello Mommy");
   }
 });
 
 $(document).ready(function() {
-  var page_1 = new WebsiteModel();
+  var page_1 = new WebsiteModel(); //WebsiteModel인스턴스
 })
+
+
+
